6b5b4370ed11fa2346d4893b3e1096a58f99f3c3
Doh Jun Hyeong <99169066+dohjh0219@users.noreply.github.com>
2022-03-17 13:53:05 +0900

    Update 125-valid-palindrome(by Slicing).py

--- 125-valid-palindrome/125-valid-palindrome(by Slicing).py
+++ 125-valid-palindrome/125-valid-palindrome(by Slicing).py
@@ -4,6 +4,6 @@ class Solution:
     def isPalindrome(self, s:str) -> bool :
         s=s.lower() # 불필요한 문자 필터링
 
-        s=re.sub('[^a-z0-9]','',s)
+        s=re.sub('[^a-z0-9]','',s)  # 정규 표현식 문자열 치환을 통해서 불필요한 문자 필터링.
 
-        return s==s[::-1]
+        return s==s[::-1]  # 문자열 슬라이싱을 통해 뒤집은 문자열 s와 같은지 비교 후 불리언으로 출력.
